0029bdfac7a86e8ef4d7f0c7f77a601236a604da
Manish Singh <manishupwala731@gmail.com>
2024-04-28 23:52:29 +0530

    first commit

---
+++ 9_loop.py
@@ -0,0 +1,12 @@
+# for loop
+# for (variable) in range in (start,end,gap)
+for i in range (0,5):
+    print(i+1)
+    
+for i in range (1,10):
+    print(i,"Manish singh")
+        
+ 
+j=int(input("Enter the number whose table want to print : "))    
+for i in range (1,11):
+    print(j,' X ',i," = ",j*i)   
